d50159def714f006b0f8fbb17b85c9b14181207b
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2018-01-08 15:21:10 -0300

    added params reader

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,12 +1,17 @@
 <template>
 	<div>
 		<h1>The User Page</h1>
+		<p>User id: {{id}}</p>
 		<button class="btn btn-primary" @click="gotoHome()">Go to home page</button>
 	</div>
 </template>
 <script>
 	export default {
-
+		data () {
+			return {
+				id: this.$route.params.id
+			}
+		},
 		methods: {
 			gotoHome () {
 				this.$router.push('/')
