044d87984e42c88fc218d6eeca0891d67c5fe240
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2018-01-08 15:11:10 -0300

    added button link to home with router action

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,3 +1,16 @@
 <template>
-    <h1>The User Page</h1>
+	<div>
+		<h1>The User Page</h1>
+		<button class="btn btn-primary" @click="gotoHome()">Go to home page</button>
+	</div>
 </template>
+<script>
+	export default {
+
+		methods: {
+			gotoHome () {
+				this.$router.push('/')
+			}
+		}
+	}
+</script>
