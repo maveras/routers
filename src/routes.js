376bc1ee31cc2e443529f2bc77ce3263f1a7d107
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2018-01-08 15:14:05 -0300

    added dinamics params to user page

--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,5 @@ import User from './components/user/User.vue'
 import home from './components/Home.vue'
 export const routes = [
  {path:'/', component: home},
- {path:'/user', component: User}
+ {path:'/user/:id', component: User}
 ]
