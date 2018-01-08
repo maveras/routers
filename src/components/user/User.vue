fd59efd7cbeccd86ace06d6daeb509feebbf240d
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2018-01-08 16:27:12 -0300

    add base routes to user

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -3,20 +3,11 @@
 		<h1>The User Page</h1>
 		<p>User id: {{id}}</p>
 		<button class="btn btn-primary" @click="gotoHome()">Go to home page</button>
+		<router-view></router-view>
 	</div>
 </template>
 <script>
 	export default {
-		data () {
-			return {
-				id: this.$route.params.id
-			}
-		},
-		watch: {
-			'$route'(to, from) {
-				this.id = to.params.id;
-			}
-		},
 		methods: {
 			gotoHome () {
 				this.$router.push('/')
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,14 @@
 import User from './components/user/User.vue'
 import home from './components/Home.vue'
+import UserDetail from './components/User/UserDetail.vue'
+import UserEdit from './components/User/UserEdit.vue'
+import UserStart from './components/User/UserStart.vue'
+
 export const routes = [
- {path:'/', component: home},
- {path:'/user/:id', component: User}
+  {path:'/', component: home},
+  {path:'/user', component: User, children: [
+    {path:'', component: UserStart},
+    {path:':id', component: UserDetail},
+    {path:':id/edit', component: UserEdit}
+  ]}
 ]
