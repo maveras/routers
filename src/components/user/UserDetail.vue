aca09a16a02b3dea200e195d76b9f07d92230211
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2018-01-11 14:42:28 -0300

    added query params to edit user link

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,6 +2,6 @@
 	<div>
 		<h3>Some User Detail</h3>
 		<p>  user has id : {{ $route.params.id}}</p>
-		<router-link tag="button" :to="{ name: 'userEdit', params: {id: $route.params.id}}" class="btn btn-primary">Edit User</router-link>
+		<router-link tag="button" :to="{ name: 'userEdit', params: {id: $route.params.id}, query: {locale:'es', q:100}}" class="btn btn-primary">Edit User</router-link>
 	</div>
 </template>
