3bda7079b82dc3312b84556355dd4ac6cbc8cdef
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2018-01-11 12:40:33 -0300

    dynamics routes by a link button

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,7 +1,6 @@
 <template>
 	<div>
 		<h1>The User Page</h1>
-		<p>User id: {{id}}</p>
 		<button class="btn btn-primary" @click="gotoHome()">Go to home page</button>
 		<router-view></router-view>
 	</div>
--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>
-		<h3>Some User Details</h3>
-		<p>user id: {{ $route.params.id}}</p>
+		<h3>Some User Detail</h3>
+		<p>  user has asssssid : {{ $route.params.id}}</p>
+		<router-link tag="button" :to="'/user/'+ $route.params.id +'/edit'" class="btn btn-primary">Edit</router-link>
 	</div>
 </template>
